8a88cce549e171e7ac36818e230069d891862e5e
med4h <adrian.meda6@gmail.com>
2025-04-29 13:01:49 -0700

    added user stories

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const port = 3000
 connectDB();
 app.use(express.json());
 
-// add snippet
+// add/create new snippet
 app.post('/snippets', async (req, res) => {
     try {
         const { language, code } = req.body;
@@ -20,6 +20,7 @@ app.post('/snippets', async (req, res) => {
     }
 });
 
+// get all snippets currently in data store
 app.get('/snippets', async (req, res) => {
     try {
         const snippets = await Snippet.find();
@@ -28,6 +29,32 @@ app.get('/snippets', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+// get snippet by id
+app.get('/snippets/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const snippet = await Snippet.findById(id);
+     if (!snippet) {
+        return res. status(404).json({ error: 'Snippet not found'});
+     }
+     res.json(snippet);
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+});
+
+//get snippets by language 
+app.get('/snippets', async (req, res) => {
+    try {
+        const { lang } = req.query;
+        const filter = lang ? { language : lang } :{};
+        const snippets = await Snippet.find(filter);
+        res.json(snippets);
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
 
 
 
