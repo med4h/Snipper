b31b1bb47446a16a2b473ef1a33d9c4ff14252bd
med4h <adrian.meda6@gmail.com>
2025-05-13 13:14:59 -0700

    hash password before saving

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bycrypt = require('bycrypt');
+
+const SALT_ROUNDS = 10;
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -7,4 +10,15 @@ const UserSchema = new mongoose.Schema({
     createdAt: { type: Date, default: Date.now },
 });
 
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        const salt = await bycrypt.genSalt(SALT_ROUNDS);
+        this.password = await bycrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    return bycrypt.compare(candidatePassword, this.password);
+}
 module.exports = mongoose.model('User', UserSchema);
